ab556441dbc615d04a9418708321a3c16412d01c
tranngocson210599 <55520239+tranngocson210599@users.noreply.github.com>
2020-10-14 18:18:58 +0700

    Update client.c

--- client.c
+++ client.c
@@ -1,40 +1,54 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
-#define PORT 8080 
-   
-int main(int argc, char const *argv[]) 
+
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define MAX 1024
+void ChatWithServer(int clientSocket) 
 { 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
+    char buffC[MAX]; 
+    char buffS[MAX]; 
+    int n; 
+    for (;;) { 
+        bzero(buffC, sizeof(buffC)); 
+        printf("Send message to Server : "); 
+        n = 0; 
+        
+        while ((buffC[n++] = getchar()) != '\n');  
+         if ((strncmp(buffC, "exit", 4)) == 0) { 
+             send(clientSocket, buffC, sizeof(buffC), 0); 
+            printf("Client Exit...\n"); 
+            break; 
+        } 
+        send(clientSocket, buffC, sizeof(buffC), 0); 
+       // read(clientSocket, buffS, sizeof(buffS));
+       // printf("Message from Server : %s", buffS);     
        
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "10.230.5.186", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
     } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
 } 
+int main() {
+  int clientSocket;
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+  char buffS[MAX];
+  char buffC[MAX] = "client";
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+  serverAddr.sin_family = AF_INET;
+
+  serverAddr.sin_port = htons(7473);
+
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+  addr_size = sizeof serverAddr;
+  connect(clientSocket, (struct sockaddr *)&serverAddr, addr_size);
+  ChatWithServer(clientSocket);
+  close(clientSocket);
+}
